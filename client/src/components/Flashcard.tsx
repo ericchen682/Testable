d7a8c17c88c57c4b4dfa1bacd1131a3435b9fdba
ericchen682 <eric.chen682@gmail.com>
2026-05-06 02:33:43 -0700

    Add correct colors to flashcard, round corners, and change size

--- client/src/components/Flashcard.tsx
+++ client/src/components/Flashcard.tsx
@@ -12,10 +12,10 @@ function Flashcard({ titleText, frontText, backText }: FlashcardProps) {
     const [isFlipped, setFlipped] = React.useState(false);
     const displayText = isFlipped ? backText : frontText;
     return(
-        <Card style={{ width: '18rem' }} onClick={() => setFlipped(!isFlipped)}>
+        <Card style={{ width: '25rem', height: '12rem', backgroundColor: "#EFE3CA", borderRadius: "1rem"}} onClick={() => setFlipped(!isFlipped)}>
             <Card.Body>
-                <Card.Title>{titleText}</Card.Title>
-                <Card.Text>{displayText}</Card.Text>
+                <Card.Title style = {{color: "#170C79"}}>{titleText}</Card.Title>
+                <Card.Text style = {{color: "#170C79"}}>{displayText}</Card.Text>
             </Card.Body>
         </Card>
     )
