cba2eccd81dadf6131900fee6043eadabb2dbea0
Aaron Zhang <Aaronzhang128@gmail.com>
2026-05-09 22:59:53 -0700

    Login Pages React + CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
