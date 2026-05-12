6a8acb9314445ec2b1585931c4a07da7bf1b8447
rixiiz <ryanliboyuan@gmail.com>
2026-05-12 14:16:46 -0700

    make and edit flashcards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
+import FlashcardView from './pages/FlashcardView'
+import FlashcardEditor from './pages/FlashcardEditor'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -13,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/flashcards/:setId" element={<FlashcardView />} />
+        <Route path="/flashcards/:setId/edit" element={<FlashcardEditor />} />
       </Routes>
     </BrowserRouter>
   );
