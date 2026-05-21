788aacd127b712d7485d5e7fc51da5b5fa36c61b
ericchen682 <eric.chen682@gmail.com>
2026-05-21 00:48:27 -0700

    create sql statements for finding user by email, id and creating user

--- server/utils/users.js
+++ server/utils/users.js
@@ -2,6 +2,10 @@
 
 const db = require()
 
+const findByEmailStmt = db.prepare('SELECT * FROM users WHERE email = ?');
+const findByIdStmt = db.prepare('SELECT * FROM USERS WHERE id = ?');
+const insertUserStmt = db.prepare('INSERT INTO users (id, email, password_hash, password_salt, created_at) VALUES (@id, @email, @passwordHash, @passwordSalt, @createdAt)');
+
 async function findUserByEmail(email) {
   
 }
